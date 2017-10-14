54deb91e8cd1f3c3711b908194c096a0afd29e41
Willixnosferatuu <guillem.reixach@e-campus.uab.cat>
2017-10-14 11:56:25 +0200

    k

--- AWESOMEBOT2.py
+++ AWESOMEBOT2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Basic example for a bot that uses inline keyboards.
-# This program is dedicated to the public domain under the CC0 license.
 
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
