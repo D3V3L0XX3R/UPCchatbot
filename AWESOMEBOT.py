e51287e234ccd3c5236b0d45da17ffe9184d799b
Luis <luisaitor.bahillo@e-campus.uab.cat>
2017-10-14 18:27:51 +0200

    Gooood

--- AWESOMEBOT.py
+++ AWESOMEBOT.py
@@ -13,7 +13,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 										level=logging.INFO)
 logger = logging.getLogger(__name__)
 def start(bot, update, user_data):
-		user_data['count'] = 1;
+		'''update.message.text = None
+		update.message.reply_text('My name is Skyblue!\nWhat is your name?')
+		while update.message.text == None:
+			pass
+		user_data['name'] = update.message.text'''
+		user_data['count'] = 1
 		keyboard = [[InlineKeyboardButton("Flight  ✈️", callback_data='1'),
 								 InlineKeyboardButton("Hotel 🏨", callback_data='2')],
 
@@ -21,10 +26,11 @@ def start(bot, update, user_data):
 
 		reply_markup = InlineKeyboardMarkup(keyboard)
 
+		#update.message.reply_text('Hi ' + user_data['name'] + '! What would you need?', reply_markup=reply_markup)
 		update.message.reply_text('Hi! What would you need?', reply_markup=reply_markup)
 
 def cancel(bot, update, user_data):
-	del user_data;
+	del user_data
 
 def button(bot, update, user_data):
 		query = update.callback_query
@@ -43,6 +49,10 @@ def button(bot, update, user_data):
 			#bot.edit_message_text(text="How much money have you at maximum?", chat_id=query.message.chat_id, message_id=query.message.message_id)
 			bot.send_message(chat_id=query.message.chat_id, text='How much money have you at maximum?')
 			#AWESOMECAR.startcar(query)
+		elif query.data == "a" or query.data == "b":
+			user_data['location'] = query.data
+			bot.send_message(chat_id=query.message.chat_id, text=user_data)
+			user_data['count'] = 4
 
 		else:
 			bot.edit_message_text(text="RILLY NIGGA",
@@ -65,9 +75,15 @@ def echo(bot, update, user_data):
 		elif user_data['count'] == 2:
 			if user_data['start'] == "3":
 				user_data['count'] = 3
-				user_data['money'] = update.message.text
-				print user_data['money']	
-			
+				user_data['money'] = update.message.text		
+				update.message.reply_text("How much days you want to rent?")	
+		elif user_data['count'] == 3:
+			if user_data['start'] == "3":
+				
+				keyboard = [[InlineKeyboardButton("Car Hire",callback_data = 'a' ),InlineKeyboardButton("Aiport Transfers",callback_data='b')]]
+				reply_markup = InlineKeyboardMarkup(keyboard)
+				update.message.reply_text('Choose your option',reply_markup=reply_markup)
+
 
 	except: 
 		update.message.reply_text("Please, write /start to awake me")
