dbeaab4e076b29874df62c52167ed53e95762410
1392633 <oriol.martinezagu@e-campus.uab.cat>
2017-10-14 11:56:24 +0200

    Comidilla

--- AWESOMEHOTEL.py
+++ AWESOMEHOTEL.py
@@ -8,12 +8,6 @@ import json
 def starthotel(bot, chat_id):
     global update_id
     bot.send_message(chat_id=chat_id, text="Hotel California")
-    for update in bot.get_updates(offset=update_id, timeout=10):
-    update_id = update.update_id + 1
-
-    if update.message:
-      
-        update.message.reply_text(update.message.text)
 
     r = requests.get('http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/FR/eur/en-US/es/us/anytime/anytime?apikey=ha129292138013702875479911846997')
 
